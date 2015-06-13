096b33946024481dc098e692d4c2d92170855ee3
jasonkalmeida <jasonkalmeida>
2015-06-13 15:12:20 -0700

    folder

--- testserver.js
+++ testserver.js
@@ -1,14 +1,9 @@
 var express = require('express');
-var path = require('path'); 
-
 var app = express();
-app.get('/public');
-app.use(express.static(__dirname + '/static'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-})
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
+
 
 console.log("Running at Port 3000");
