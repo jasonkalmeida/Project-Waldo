6dbcc428246abbb52077306851af84a661b64dc2
jasonkalmeida <jasonkalmeida>
2015-06-13 23:53:14 -0700

    Keys and shit

--- testserver.js
+++ testserver.js
@@ -12,9 +12,16 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.post('/myaction', function(req, res) {
-    var name = req.body.ownEmail;
-
-	console.log(name);
+    var memail = req.body.ownEmail;
+	var cemail = req.body.coEmail;
+	var type = req.body.trip;
+	
+	console.log(memail);
+	console.log(cemail);
+	console.log(type);
+	
+	res.sendFile(__dirname + '/public/planner.html');
+	
 });
 
 
