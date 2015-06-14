24fad9c96e98365abdaca08c36972e681cf24c35
jasonkalmeida <jasonkalmeida>
2015-06-13 18:53:04 -0700

    IT'S ALIVE

--- testserver.js
+++ testserver.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
 
+
+
+app.post('/myaction', function(req, res) {
+    var name = req.body.ownEmail;
+
+	console.log(name);
+});
+
+
+app.listen(process.env.PORT || 3000);
 
 console.log("Running at Port 3000");
