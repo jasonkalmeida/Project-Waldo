4a78fecba12d9578b3dc685db30848ef1d6c695d
samaj94 <samaj94@gmail.com>
2015-06-14 11:18:50 -0700

    bla

--- bars.js
+++ bars.js
@@ -7,9 +7,14 @@ var yelp = require("yelp").createClient({
 
 // See http://www.yelp.com/developers/documentation/v2/search_api
 yelp.search({term: "bars", location: "Montreal", limit: 3, sort: 2}, function(error, data) {
-  console.log(error);
-  for(i=0; i < 3; i++){
-	console.log(data.businesses[i].name);
-	console.log(data.businesses[i].rating);
-  }
-});	
+var items = [
+	data.businesses[0].name,
+	data.businesses[1].name,
+	data.businesses[2].name
+]
+});
+
+yelp.display(), function(){
+	console.log(data.businesses[0].name);
+}
+
